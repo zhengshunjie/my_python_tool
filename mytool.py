89026b43dc76de0135ea137b2af77d7e8c0ab21e
shunjie.zheng <shunjie.zheng@neucore.com>
2018-12-06 17:35:27 +0800

    delete creat_dir

--- mytool.py
+++ mytool.py
@@ -34,22 +34,26 @@ def type_file_name(file_dir,type):
     files_folder_path.sort()
     return files_full_path,files_folder_path,files_name
 
-def create_folder(root, folder_name):
-    #root Current root directory
-    #folder To create directory names
-    #return Path to create files root+folder_name
-    if not os.path.exists(root):
-        print ('root path not exist...')
-        sys.exit()
-    if not os.path.exists(folder_name):
-        os.mkdir(root+'/'+folder_name)
-        print ('create ')
-        print (root+'/'+folder_name)
-        print ('...')
-        print ('\n')
-    else:
-        print ('folder_name exist...')
-    return root + '/' + folder_name
+def list_dir(dir):
+    path = os.listdir(dir)
+    return path
+
+# def create_folder(root, folder_name):
+#     #root Current root directory
+#     #folder To create directory names
+#     #return Path to create files root+folder_name
+#     if not os.path.exists(root):
+#         print ('root path not exist...')
+#         sys.exit()
+#     if not os.path.exists(folder_name):
+#         os.mkdir(root+'/'+folder_name)
+#         print ('create ')
+#         print (root+'/'+folder_name)
+#         print ('...')
+#         print ('\n')
+#     else:
+#         print ('folder_name exist...')
+#     return root + '/' + folder_name
 def create_folder(path):
     if not os.path.exists(path):
         os.mkdir(path)
